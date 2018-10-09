4108bc500aa62b1d25978d515be6b45eccd90f15
Douglas Uretsky <douglasuretsky@gmail.com>
2018-10-09 20:44:40 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
     artist_name = parsed[0]
     title = parsed[1]
     new_song = self.new(title)
-    # new_artist = Artist.new(artist_name)
+    new_artist = Artist.new(artist_name)
     new_song.artist = Artist.find_or_create_by_name(artist_name)
     new_artist.save
     new_song
